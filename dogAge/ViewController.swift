fcb4e48c2182ae3ad968dbf9a85c07d788f2ebaa
Diego Collares <diego.collares@gmail.com>
2017-02-24 12:09:18 -0300

    Initial Commit

---
+++ dogAge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dogAge
+//
+//  Created by Diego Collares on 24/02/17.
+//  Copyright © 2017 Diego Collares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
